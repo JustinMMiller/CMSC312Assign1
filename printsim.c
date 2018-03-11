d658b95d7509c03d6ccbcc72e6a977b38c41e249
Justin Miller <justintime145@gmail.com>
2018-03-10 22:27:19 -0500

    Almost there, not terminating at the end

--- printsim.c
+++ printsim.c
@@ -27,8 +27,17 @@ sem_t read_sem;
 sem_t write_sem;
 int start = 0;
 int end = 0;
-int flag = 1;
+int stop = 0;
 int size = 0;
+
+void signal_handler(int signal)
+{
+	if(signal == SIGINT)
+	{
+		printf("Shutting down...\n");
+		stop = 1;
+	}
+}
 
 void addJob(int jobsize, int id)
 {
@@ -36,7 +45,7 @@ void addJob(int jobsize, int id)
 	sem_wait(&lock_sem);
 	struct Job job = new_job;
 	job.size = jobsize;
-	job.valid = 1;
+	job.valid = id;
 	job.submitter = id;
 	queue[end] = job;
 	end++;
@@ -47,32 +56,64 @@ void addJob(int jobsize, int id)
 	sem_post(&read_sem);
 }
 
-void printJob()
+int printJob(int id)
 {
+	int ret = -1;
+	printf("Thread %i is waiting to read\n");
 	sem_wait(&read_sem);
 	sem_wait(&lock_sem);
-	usleep(queue[start].size);
-	start++;
-	start %= 15;
-	size--;
+	printf("queue[start].valid : %i\n");
+	printf("Thread %i processing %i job size %i", id, queue[start].submitter, queue[start].size);
+	if(queue[start].valid != -1)
+	{
+		ret = queue[start].size;
+		start++;
+		start %= 15;
+		size--;
+	}
 	if(size > 0) sem_post(&read_sem);
 	sem_post(&lock_sem);
 	sem_post(&write_sem);
-	
+	return ret;
 }
 
 void consumer_func(int num)
 {
-
+	int flag = 1;
+	if(stop)return;
+	while(flag == 1&& !stop)
+	{
+		int var = printJob(num);
+		printf("Consumer %i just processed a job\n", num);
+		if(var == -1)flag = 0;
+		sleep(2);
+	}
+}
+//https://stackoverflow.com/questions/7797664/what-is-the-most-correct-way-to-generate-random-numbers-in-c-with-pthread
+//STACK OVERFLOW
+size_t random_range(int low, int high)
+{
+	unsigned short state[3];
+	unsigned int seed = time(NULL) + (unsigned int)pthread_self();
+	memcpy(state, &seed, sizeof(seed));
+	return low + nrand48(state) % (high - low);
 }
 
 void producer_func(int num)
 {
-
+	int i = 0;
+	int althor = random_range(1, 20);
+	for(; i < althor && !stop; i++)
+	{
+		addJob(random_range(100, 1000), num);
+		printf("Producer %i added a job\n", num);
+		sleep(random_range(1, 2));
+	}
 }
 
 int main(int argc, char ** argv)
 {
+	signal(SIGINT, signal_handler);
 	srand(time(0));
 	int j = atoi(argv[1]);
 	int k = atoi(argv[2]);
@@ -84,7 +125,20 @@ int main(int argc, char ** argv)
 	int i;
 	for(i = 0; i < k; i++)
 	{
-		pthread_create(&consumers[i], NULL, consumer_func, (void*)i);
+		pthread_create(&consumers[i], NULL, consumer_func, (void*)(i+1));
+	}
+	for(i = 0; i < j; i++)
+	{
+		pthread_create(&producers[i], NULL, producer_func, (void*)(i+1));
+	}
+	for(i = 0; i < k; i++)
+	{
+		pthread_join(producers[i], NULL);
+	}
+	addJob(-1, -1);
+	for(i = 0; i < j; i++)
+	{
+		pthread_join(consumers[i], NULL);
 	}
 	return 0;
 }
