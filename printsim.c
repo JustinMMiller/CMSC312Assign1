16b4baad68ae548e009fec2c3fb536e9ce837819
Justin Miller <justintime145@gmail.com>
2018-03-11 18:00:12 -0400

    What is even going on

--- printsim.c
+++ printsim.c
@@ -27,7 +27,7 @@ sem_t write_sem;
 int start = 0;
 int end = 0;
 int stop = 0;
-int size = 0;
+
 
 void signal_handler(int signal)
 {
@@ -35,41 +35,46 @@ void signal_handler(int signal)
 	{
 		printf("Shutting down...\n");
 		stop = 1;
+		sem_post(&read_sem);
+		sem_post(&write_sem);
+		sem_post(&lock_sem);
 	}
 }
 
 void addJob(int jobsize, int id)
-{
+{	
+	printf("Producer %i wants to add a job\n", id);
 	sem_wait(&write_sem);
 	sem_wait(&lock_sem);
-	struct Job job = new_job;
-	job.size = jobsize;
-	job.submitter = id;
-	queue[end] = job;
-	end++;
-	end %= 15;
-	size++;
-	if(size < 15)sem_post(&write_sem);
-	sem_post(&lock_sem);
+	if(!stop)
+	{
+		printf("Producer %i adding a job\nStart index : %i End index %i\n", id, start, end);
+		struct Job job = new_job;
+		job.size = jobsize;
+		job.submitter = id;
+		queue[end%15] = job;
+		end++;
+	}
+	if(end - start < 2)sem_post(&write_sem);
 	sem_post(&read_sem);
+	sem_post(&lock_sem);
 }
 
 int printJob(int id)
 {
 	int ret = -1;
-	printf("Thread %i is waiting to read\n", num);
+	printf("Consumer %i is waiting to read\n", id);
 	sem_wait(&read_sem);
 	sem_wait(&lock_sem);
-	printf("queue[start].submitter : %i\n", queue[start].submitter);
-	printf("Thread %i processing job size %i\n", id, queue[start].size);
-	if(queue[start].submitter != -1)
+	printf("Consumer %i processing job size %i end - start %i\n", id, queue[start%15].size, end-start);
+	printf("Job Submitter : %i\n", queue[start%15].submitter);
+	if(queue[start%15].size > 0)
 	{
-		ret = queue[start].size;
+		ret = queue[start%15].size;
 		start++;
-		start %= 15;
-		size--;
 	}
-	if(size > 0) sem_post(&read_sem);
+	printf("Consumer %i removed a job\nStart index : %i End index %i\n", id, start, end);
+	if(start < end) sem_post(&read_sem);
 	sem_post(&lock_sem);
 	sem_post(&write_sem);
 	return ret;
@@ -78,14 +83,14 @@ int printJob(int id)
 void consumer_func(int num)
 {
 	int flag = 1;
-	if(stop)return;
-	while(flag == 1&& !stop)
+	while(flag == 1 && !stop)
 	{
+		if(stop)return;
 		int var = printJob(num);
-		printf("Consumer %i just processed a job\n", num);
 		if(var == -1)flag = 0;
-		usleep(var*1000);
+		else{sleep((var % 5)+1);}
 	}
+	printf("Consumer %i is DYING\n\n", num);
 }
 //https://stackoverflow.com/questions/7797664/what-is-the-most-correct-way-to-generate-random-numbers-in-c-with-pthread
 //STACK OVERFLOW
@@ -100,12 +105,12 @@ size_t random_range(int low, int high)
 void producer_func(int num)
 {
 	int i = 0;
-	int althor = random_range(1, 20);
+	int althor = random_range(1, 5);
 	for(; i < althor && !stop; i++)
 	{
+		if(stop)return;
 		addJob(random_range(100, 1000), num);
-		printf("Producer %i added a job\n", num);
-		usleep(random_range(100, 1000)*1000);
+		sleep(random_range(1, 3));
 	}
 }
 
@@ -118,7 +123,7 @@ int main(int argc, char ** argv)
 	pthread_t producers[j];
 	pthread_t consumers[k];
 	sem_init(&lock_sem, 0, 1);
-	sem_init(&read_sem, 0, 1);
+	sem_init(&read_sem, 0, 0);
 	sem_init(&write_sem, 0, 1);
 	int i;
 	for(i = 0; i < k; i++)
@@ -129,12 +134,12 @@ int main(int argc, char ** argv)
 	{
 		pthread_create(&producers[i], NULL, producer_func, (void*)(i+1));
 	}
-	for(i = 0; i < k; i++)
+	for(i = 0; i < j; i++)
 	{
 		pthread_join(producers[i], NULL);
 	}
 	addJob(-1, -1);
-	for(i = 0; i < j; i++)
+	for(i = 0; i < k; i++)
 	{
 		pthread_join(consumers[i], NULL);
 	}
