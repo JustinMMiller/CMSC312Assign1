3c31abb7f4ba524fc7c46dce022779febd5bdf31
Justin Miller <justintime145@gmail.com>
2018-03-12 15:09:13 -0400

    SJF WORKS

--- printsim.c
+++ printsim.c
@@ -9,6 +9,7 @@
 #include<signal.h>
 #include<semaphore.h>
 #include<pthread.h>
+#include<limits.h>
 
 struct Job 
 {
@@ -17,7 +18,7 @@ struct Job
 };
 struct Job new_job;
 
-struct Job quit_job = { -1, -1};
+struct Job dummy_job = { INT_MAX, -2};
 
 struct Job queue[15];
 
@@ -25,8 +26,55 @@ sem_t lock_sem;
 sem_t read_sem;
 sem_t write_sem;
 int start = 0;
-int end = 0;
+int qsize = 0;
 int stop = 0;
+
+void init_queue()
+{
+	int i;
+	for(i = 0; i < 15; i++)
+	{
+		queue[i].size = dummy_job.size;
+		queue[i].submitter = dummy_job.submitter;
+		printf("i %i size %i submitter %i\n", i, queue[i].size, queue[i].submitter);
+	}
+}
+
+void insert_into_queue(int jsize, int submit)
+{
+	int i;
+	for(i = 0; i < 15; i++)
+	{
+		if(queue[i].submitter == -2)
+		{
+			printf("Found empty slot for %i job size %i\n", submit, jsize);
+			queue[i].submitter = submit;
+			queue[i].size = jsize;
+			qsize++;
+			printf("i %i size %i submitter %i\n", i, queue[i].size, queue[i].submitter);
+			return;
+		}
+	}
+}
+
+int remove_from_queue(int id)
+{
+	int i, ind = 0;
+	for(i = 0; i < 15; i++)
+	{
+		if(queue[i].size < queue[ind].size) ind = i;
+	}
+	printf("Consumer %i removing a job submitted by Consumer %i size : %i\n", id, queue[ind].submitter, queue[ind].size);
+	int joblen = queue[ind].size;
+	if(queue[ind].submitter != -1)
+	{
+		queue[ind].size = INT_MAX;
+		queue[ind].submitter = -2;
+		qsize--;
+	}
+	printf("qsize after removing job : %i\n", qsize);
+	return joblen;
+}
 
 
 void signal_handler(int signal)
@@ -48,14 +96,8 @@ void addJob(int jobsize, int id)
 	sem_wait(&lock_sem);
 	if(!stop)
 	{
-		printf("Producer %i adding a job\nStart index : %i End index %i\n", id, start, end);
-		struct Job job = new_job;
-		job.size = jobsize;
-		job.submitter = id;
-		queue[end%15] = job;
-		end++;
+		insert_into_queue(jobsize, id);
 	}
-	if(end - start < 2)sem_post(&write_sem);
 	sem_post(&read_sem);
 	sem_post(&lock_sem);
 }
@@ -66,15 +108,7 @@ int printJob(int id)
 	printf("Consumer %i is waiting to read\n", id);
 	sem_wait(&read_sem);
 	sem_wait(&lock_sem);
-	printf("Consumer %i processing job size %i end - start %i\n", id, queue[start%15].size, end-start);
-	printf("Job Submitter : %i\n", queue[start%15].submitter);
-	if(queue[start%15].size > 0)
-	{
-		ret = queue[start%15].size;
-		start++;
-	}
-	printf("Consumer %i removed a job\nStart index : %i End index %i\n", id, start, end);
-	if(start < end) sem_post(&read_sem);
+	ret = remove_from_queue(id);
 	sem_post(&lock_sem);
 	sem_post(&write_sem);
 	return ret;
@@ -87,9 +121,10 @@ void consumer_func(int num)
 	{
 		if(stop)return;
 		int var = printJob(num);
-		if(var == -1)flag = 0;
+		if(var == 2000)flag = 0;
 		else{sleep((var % 5)+1);}
 	}
+	sem_post(&read_sem);
 	printf("Consumer %i is DYING\n\n", num);
 }
 //https://stackoverflow.com/questions/7797664/what-is-the-most-correct-way-to-generate-random-numbers-in-c-with-pthread
@@ -116,6 +151,7 @@ void producer_func(int num)
 
 int main(int argc, char ** argv)
 {
+	init_queue();
 	signal(SIGINT, signal_handler);
 	srand(time(0));
 	int j = atoi(argv[1]);
@@ -124,7 +160,7 @@ int main(int argc, char ** argv)
 	pthread_t consumers[k];
 	sem_init(&lock_sem, 0, 1);
 	sem_init(&read_sem, 0, 0);
-	sem_init(&write_sem, 0, 1);
+	sem_init(&write_sem, 0, 15);
 	int i;
 	for(i = 0; i < k; i++)
 	{
@@ -138,7 +174,7 @@ int main(int argc, char ** argv)
 	{
 		pthread_join(producers[i], NULL);
 	}
-	addJob(-1, -1);
+	addJob(2000, -1);
 	for(i = 0; i < k; i++)
 	{
 		pthread_join(consumers[i], NULL);
